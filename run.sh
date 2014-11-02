cd8ad502e33a1bdf7e3b1faf13c4aadac651e56e
Vikrant Varma <vikrant.varma94@gmail.com>
2014-11-02 08:41:58 +0530

    Initial commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Starting xi-core."
+(
+    unset http_proxy && unset https_proxy
+    cd xi-core
+    grunt start 2>&1 >> "../logs/xi-core.log"
+) &
+
+while read agent; do
+    [ -z "$agent" ] && continue # skip empty lines
+    [[ $agent == \#* ]] && continue # skip comments
+    (
+        unset http_proxy && unset https_proxy
+        cd "agents/$agent"
+        node index.js 2>&1 >> "../../logs/$agent.log"
+    ) &
+    echo "Starting agent: $agent."
+done < scripts/agents
+
+echo "Xi started"
+wait
