d366e9fe74f4bb62260f54a5ea543956d9f31cc4
Vikrant Varma <vikrant.varma94@gmail.com>
2015-03-24 21:00:24 +0530

    Make update.sh more robust

--- update.sh
+++ update.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x
+SCRIPTS_DIR="scripts"
+AGENTS_FILE="scripts/agents"
+
+trap "kill 0" SIGINT # kill all subprocesses
+
+set -e # exit script if any command errors out
 
 git submodule init
 git submodule update
 
+if [ ! -f $AGENTS_FILE ]; then
+    (set -x && exec cp "$SCRIPTS_DIR/agents.sample" $AGENTS_FILE)
+fi
+
 while read agent; do
-    set +x
     [ -z "$agent" ] && continue # skip empty lines
     [[ $agent == \#* ]] && continue # skip comments
-    set -x
-    (cd "agents/$agent" && exec npm install --production)
-done < scripts/agents
+    (set -x && cd "agents/$agent" && exec npm install --production)
+done < $AGENTS_FILE
 
 echo "Update complete."
